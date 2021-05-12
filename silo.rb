e51f4273f7e72938734a663ab079d874561c91f8
Gunther H. Weber <GHWeber@lbl.gov>
2021-05-11 20:08:22 -0700

    Updated URL for Silo

--- silo.rb
+++ silo.rb
@@ -1,7 +1,7 @@
 class Silo < Formula
   desc "LLNL mesh and field Silo I/O library. Allows creating databases for VisIt."
   homepage "https://wci.llnl.gov/simulation/computer-codes/silo"
-  url "https://wci.llnl.gov/content/assets/docs/simulation/computer-codes/silo/silo-4.10.2/silo-4.10.2-bsd.tar.gz"
+  url "https://wci.llnl.gov/sites/wci/files/2021-01/silo-4.10.2-bsd.tgz"
   sha256 "4b901dfc1eb4656e83419a6fde15a2f6c6a31df84edfad7f1dc296e01b20140e"
   revision 4
 
